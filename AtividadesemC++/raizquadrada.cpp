9c0525dca66444f9a2d2a8e7a65c938b77ee3e0e
Rodrigo Moraes <78232612+RodrigoMoraes28@users.noreply.github.com>
2021-11-21 11:10:45 -0300

    Add files via upload

---
+++ AtividadesemC++/raizquadrada.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main(){
+    
+    int N;
+    float num,raiz;
+    
+    cin >> N ;
+    
+    int i;
+    
+    for(i=0 ; i<N; i++){
+    	cout << N << endl;
+    }  
+	raiz = sqrt(num);   
+
+ return 0;   
+}
