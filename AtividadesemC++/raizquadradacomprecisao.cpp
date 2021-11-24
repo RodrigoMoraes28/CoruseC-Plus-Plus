c3dfd9eeea045740b7fe8d920a4b85a281431a19
Rodrigo Moraes <78232612+RodrigoMoraes28@users.noreply.github.com>
2021-11-23 23:59:34 -0300

    Update raizquadradacomprecisao.cpp

--- AtividadesemC++/raizquadradacomprecisao.cpp
+++ AtividadesemC++/raizquadradacomprecisao.cpp
@@ -9,7 +9,7 @@ int main(){
 
     cin >> n;
 
-    for(int i=0; i<n; i++){ // esse for aqui eh para ler os numeros n vezes e depois ele executa
+    for(int i=0; i<n; i++){ // esse for aqui eh para ler os numeros n vezes e depois ele executa os comandos abaixo dele
         cin >> num;
         raiz = sqrt(num);
     	cout << fixed << setprecision (4); //raiz quadrada porem escolhendo a precisão do resultado da raiz, ae no caso foi de 4 , mas poderia ser de qualquer outro numero
