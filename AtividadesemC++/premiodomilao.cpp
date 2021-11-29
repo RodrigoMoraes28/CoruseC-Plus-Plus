09e44c390ed29928c1d8b54459bc5d6c774d8aa6
Rodrigo Moraes <78232612+RodrigoMoraes28@users.noreply.github.com>
2021-11-28 22:32:52 -0300

    Add files via upload

---
+++ AtividadesemC++/premiodomilao.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main (){
+
+    int sequencia,numero,maior,cont=0,resposta;
+
+    cin >> sequencia ;
+
+    for (int i=0;i<sequencia;i++){
+        cin >> numero;
+        if ( i = numero)
+        resposta = max(resposta,cont);
+        cont = 0;
+
+
+    }
+        cout << cont;
+return 0;
+}
