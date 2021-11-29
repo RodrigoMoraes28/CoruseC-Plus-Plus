450f058977b713da1b0cbddd30e925762083901b
Rodrigo Moraes <78232612+RodrigoMoraes28@users.noreply.github.com>
2021-11-28 22:47:19 -0300

    Update premiodomilao.cpp

--- AtividadesemC++/premiodomilao.cpp
+++ AtividadesemC++/premiodomilao.cpp
@@ -6,14 +6,11 @@ int main (){
     int sequencia,numero,maior,cont=0,resposta;
 
     cin >> sequencia ;
-
     for (int i=0;i<sequencia;i++){
         cin >> numero;
         if ( i = numero)
         resposta = max(resposta,cont);
         cont = 0;
-
-
     }
         cout << cont;
 return 0;
