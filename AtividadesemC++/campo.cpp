2294eaf9a5358d198aa673daad1b68525f60feca
Rodrigo Moraes <78232612+RodrigoMoraes28@users.noreply.github.com>
2021-12-02 23:58:24 -0300

    Add files via upload

---
+++ AtividadesemC++/campo.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    int N;
+    int campo[55];
+
+    cin >> N;
+
+    for (int i=0;i<N;i++){
+         cin >> campo[i];
+    }
+    for (int i=0;i<N;i++){
+        int quantidade = 0;
+        quantidade = quantidade +campo [i];
+
+        if (i < N-1){
+        quantidade = quantidade +campo [i+1];
+        }
+        if(i>0){
+        quantidade = quantidade +campo [i-1];
+    }
+        cout << quantidade << endl;
+
+    }
+
+return 0;
+}
